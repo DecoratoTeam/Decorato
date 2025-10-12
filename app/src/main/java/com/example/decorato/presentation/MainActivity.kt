a1fc5955ce3e251118f73cd19f97a9e12cc56df8
saraashraf11 <saraashraf1822004@gmail.com>
2025-10-12 23:49:56 +0300

    refactor: move MainActivity.kt to presentation layer

--- app/src/main/java/com/example/decorato/presentation/MainActivity.kt
+++ app/src/main/java/com/example/decorato/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.decorato
+package com.example.decorato.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,37 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.decorato.ui.theme.DecoratoTheme
+import com.example.decorato.presentation.theme.DecoratoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DecoratoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DecoratoTheme {
-        Greeting("Android")
     }
 }
