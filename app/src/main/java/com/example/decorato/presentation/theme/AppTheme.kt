391b7eb73e1372fec349e7310ed293da6e4a3610
saraashraf11 <saraashraf1822004@gmail.com>
2025-10-16 23:05:49 +0300

    feature: add AppTheme to handle app design system

--- app/src/main/java/com/example/decorato/presentation/theme/AppTheme.kt
+++ app/src/main/java/com/example/decorato/presentation/theme/AppTheme.kt
@@ -1,4 +1,23 @@
 package com.example.decorato.presentation.theme
 
-class AppTheme {
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.Color
+import com.example.decorato.presentation.theme.colors.DecoratoAppColors
+import com.example.decorato.presentation.theme.colors.LocalDecoratoAppColors
+import com.example.decorato.presentation.theme.textStyle.DecoratoTextStyle
+import com.example.decorato.presentation.theme.textStyle.LocalDecoratoTextStyle
+
+typealias GradientType = @Composable () -> List<Color>
+
+typealias ColorType = @Composable () -> Color
+
+object AppTheme {
+    val color: DecoratoAppColors
+        @Composable @ReadOnlyComposable
+        get() = LocalDecoratoAppColors.current
+
+    val textStyle: DecoratoTextStyle
+        @Composable @ReadOnlyComposable
+        get() = LocalDecoratoTextStyle.current
 }
