f193c8a03574a8e3574b77c622aac871c74ce447
saraashraf11 <saraashraf1822004@gmail.com>
2025-11-24 16:26:04 +0200

    feature: add DecoratoApp and exceptions

---
+++ app/src/main/java/com/example/decorato/domain/exceptions/DecoratoException.kt
@@ -0,0 +1,25 @@
+package com.example.decorato.domain.exceptions
+
+open class DecoratoException : Exception()
+
+open class NetworkException : DecoratoException()
+
+class UnknownException : NetworkException()
+
+class NoInternetException : NetworkException()
+
+class ServerErrorException : NetworkException()
+
+open class AuthenticationException : NetworkException()
+
+class AccountDisabledException : AuthenticationException()
+
+class InvalidCredentialsException : AuthenticationException()
+
+class VerificationRequiredException : AuthenticationException()
+
+class InvalidSessionException : AuthenticationException()
+
+class AccessDeniedException : AuthenticationException()
+
+class AccessRestrictedException : AuthenticationException()
