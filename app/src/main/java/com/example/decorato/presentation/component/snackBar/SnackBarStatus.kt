794b7d295c9f9301e3e47ccb96e7f4126e383b0b
saraashraf11 <saraashraf1822004@gmail.com>
2025-11-24 16:24:49 +0200

    feature: add SnackBar

---
+++ app/src/main/java/com/example/decorato/presentation/component/snackBar/SnackBarStatus.kt
@@ -0,0 +1,23 @@
+package com.example.decorato.presentation.component.snackBar
+
+import androidx.annotation.DrawableRes
+import com.example.decorato.R
+import com.example.decorato.presentation.theme.AppTheme
+import com.example.decorato.presentation.theme.ColorType
+
+enum class SnackBarStatus(
+    @DrawableRes val icon: Int,
+    val iconTintColor: ColorType,
+    val dropShadowColor: ColorType,
+) {
+    Success(
+        R.drawable.ic_thumbs_up,
+        iconTintColor = { AppTheme.color.greenAccent },
+        dropShadowColor = { AppTheme.color.successSnackBarShadow },
+    ),
+    Failure(
+        R.drawable.ic_thumbs_down,
+        iconTintColor = { AppTheme.color.redAccent },
+        dropShadowColor = { AppTheme.color.failureSnackBarShadow },
+    ),
+}
