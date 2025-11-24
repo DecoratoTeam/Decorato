03cd795738b33ceddce1243afcb23691104f9e44
saraashraf11 <saraashraf1822004@gmail.com>
2025-11-24 23:20:09 +0200

    update: delete action bar

--- app/src/main/java/com/example/decorato/presentation/MainActivity.kt
+++ app/src/main/java/com/example/decorato/presentation/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.decorato.presentation.navigation.NavGraph
 import com.example.decorato.presentation.navigation.Route
