bd0c2cdc8f38c8848a533440a6ceaf64d6dbd2a3
saraashraf11 <saraashraf1822004@gmail.com>
2025-11-30 23:00:07 +0200

    feature: add register module in di

--- app/src/main/java/com/example/decorato/di/useCaseModule.kt
+++ app/src/main/java/com/example/decorato/di/useCaseModule.kt
@@ -7,6 +7,7 @@ import com.example.decorato.domain.useCase.authentication.GetsSessionTypeUseCase
 import com.example.decorato.domain.useCase.authentication.LoginAsGuestUseCase
 import com.example.decorato.domain.useCase.authentication.LoginWithPasswordUseCase
 import com.example.decorato.domain.useCase.authentication.LogoutUseCase
+import com.example.decorato.domain.useCase.authentication.RegisterUseCase
 import com.example.decorato.domain.useCase.preferences.GetOnboardingStatusUseCase
 import com.example.decorato.domain.useCase.preferences.ManageAppThemeUseCase
 import com.example.decorato.domain.useCase.preferences.ManageLocaleLanguageUseCase
@@ -22,42 +23,56 @@ import dagger.hilt.android.components.ViewModelComponent
 object UseCaseModule {
 
     @Provides
-    fun provideManageRestrictionLevelUseCase(appPreferencesRepository: AppPreferencesRepository): ManageRestrictionLevelUseCase =
-        ManageRestrictionLevelUseCase(appPreferencesRepository)
+    fun provideRegisterUseCase(
+        authenticationRepository: AuthenticationRepository
+    ): RegisterUseCase = RegisterUseCase(authenticationRepository)
+
+    @Provides
+    fun provideManageRestrictionLevelUseCase(
+        appPreferencesRepository: AppPreferencesRepository
+    ): ManageRestrictionLevelUseCase = ManageRestrictionLevelUseCase(appPreferencesRepository)
 
 
     @Provides
-    fun provideGetsSessionType(authenticationRepository: AuthenticationRepository): GetsSessionTypeUseCase =
-        GetsSessionTypeUseCase(authenticationRepository)
+    fun provideGetsSessionType(
+        authenticationRepository: AuthenticationRepository
+    ): GetsSessionTypeUseCase = GetsSessionTypeUseCase(authenticationRepository)
 
     @Provides
-    fun provideGetOnboardingStatusUseCase(appPreferencesRepository: AppPreferencesRepository): GetOnboardingStatusUseCase =
-        GetOnboardingStatusUseCase(appPreferencesRepository)
+    fun provideGetOnboardingStatusUseCase(
+        appPreferencesRepository: AppPreferencesRepository
+    ): GetOnboardingStatusUseCase = GetOnboardingStatusUseCase(appPreferencesRepository)
 
     @Provides
-    fun provideSetOnboardingCompletedUseCase(appPreferencesRepository: AppPreferencesRepository): SetOnboardingCompletedUseCase =
-        SetOnboardingCompletedUseCase(appPreferencesRepository)
+    fun provideSetOnboardingCompletedUseCase(
+        appPreferencesRepository: AppPreferencesRepository
+    ): SetOnboardingCompletedUseCase = SetOnboardingCompletedUseCase(appPreferencesRepository)
 
 
     @Provides
-    fun provideSetCurrentLanguage(appPreferencesRepository: AppPreferencesRepository): ManageLocaleLanguageUseCase =
-        ManageLocaleLanguageUseCase(appPreferencesRepository)
+    fun provideSetCurrentLanguage(
+        appPreferencesRepository: AppPreferencesRepository
+    ): ManageLocaleLanguageUseCase = ManageLocaleLanguageUseCase(appPreferencesRepository)
 
     @Provides
-    fun provideLoginAsGuestUseCase(authenticationRepository: AuthenticationRepository): LoginAsGuestUseCase =
-        LoginAsGuestUseCase(authenticationRepository)
+    fun provideLoginAsGuestUseCase(
+        authenticationRepository: AuthenticationRepository
+    ): LoginAsGuestUseCase = LoginAsGuestUseCase(authenticationRepository)
 
     @Provides
-    fun provideManageAppThemeUseCase(repo: AppPreferencesRepository): ManageAppThemeUseCase =
-        ManageAppThemeUseCase(repo)
+    fun provideManageAppThemeUseCase(
+        repo: AppPreferencesRepository
+    ): ManageAppThemeUseCase = ManageAppThemeUseCase(repo)
 
     @Provides
-    fun provideLoginWithPasswordUseCase(authenticationRepository: AuthenticationRepository): LoginWithPasswordUseCase =
-        LoginWithPasswordUseCase(authenticationRepository)
+    fun provideLoginWithPasswordUseCase(
+        authenticationRepository: AuthenticationRepository
+    ): LoginWithPasswordUseCase = LoginWithPasswordUseCase(authenticationRepository)
 
     @Provides
-    fun provideLogoutUseCase(authenticationRepository: AuthenticationRepository): LogoutUseCase =
-        LogoutUseCase(authenticationRepository)
+    fun provideLogoutUseCase(
+        authenticationRepository: AuthenticationRepository
+    ): LogoutUseCase = LogoutUseCase(authenticationRepository)
 
 }
 
