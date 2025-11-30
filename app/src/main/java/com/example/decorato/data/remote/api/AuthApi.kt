c5e7147f6b43aa33b374775f775c59020cdcf234
saraashraf11 <saraashraf1822004@gmail.com>
2025-11-30 22:48:15 +0200

    feature: add AuthApi.kt

---
+++ app/src/main/java/com/example/decorato/data/remote/api/AuthApi.kt
@@ -0,0 +1,15 @@
+package com.example.decorato.data.remote.api
+
+import com.example.decorato.data.remote.dto.RegisterRequestDto
+import com.example.decorato.data.remote.dto.RegisterResponseDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthApi {
+
+    @POST("Auth/Register")
+    suspend fun register(
+        @Body request: RegisterRequestDto
+    ): Response<RegisterResponseDto>
+}
