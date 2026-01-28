7c6907552e7c7308f9c753a8c1c875acad392781
saraashraf11 <saraashraf1822004@gmail.com>
2026-01-28 20:50:38 +0200

    feature: add GenerateScreen and it's Route

---
+++ app/src/main/java/com/example/decorato/presentation/screens/generate/generateScreenRoute.kt
@@ -0,0 +1,11 @@
+package com.example.decorato.presentation.screens.generate
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.decorato.presentation.navigation.Route
+
+fun NavGraphBuilder.generateScreenRoute() {
+    composable<Route.Tab.Generate> {
+        GenerateScreen()
+    }
+}
